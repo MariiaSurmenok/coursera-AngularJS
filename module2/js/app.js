cc6048bae939d9d33803c20e3bdb4c50699c2656
Mariia <mariasurmenok@gmail.com>
2017-02-26 16:56:23 -0800

    added initial structure and custom service to angular

---
+++ module2/js/app.js
@@ -0,0 +1,62 @@
+(function() {
+	'use strict';
+
+	angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyController', ToBuyController)
+	.controller('AlreadyBoughtController', AlreadyBoughtController)
+	.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+	ToBuyController.$inject = ['ShoppingListCheckOffService'];
+	function ToBuyController(ShoppingListCheckOffService) {
+		buyList = this;
+	}
+
+	AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+	function AlreadyBoughtController(ShoppingListCheckOffService) {
+		boughtList = this;
+	}
+
+	function ShoppingListCheckOffService() {
+		var service = this;
+
+		//arrays hold to buy and bought items
+		var boughtItems = [];
+		var toBuyItems = [
+			{
+				name: 'croissant',
+      			quantity: 5
+			},
+			{
+				name: 'milk',
+      			quantity: 1
+			},
+			{
+				name: 'eggs',
+      			quantity: 12
+			},
+			{
+				name: 'coffee',
+      			quantity: 2
+			},
+			{
+				name: 'apple',
+      			quantity: 4
+			}
+
+		];
+
+		service.buyItem = function(itemIndex) {
+			item = toBuyItems.splice(itemIndex, 1)[0]
+			boughtItems.push(item);
+		}
+
+		service.getBoughtItems() {
+			return boughtItems;
+		}
+
+		service.getToBuyItems() {
+			return toBuyItems;
+		}
+	}
+
+})();
